26b5ba6ad0f0b8a2e5a2d16b59393f412aa0fe62
Kasia Biernat <kasia.biernat@hyland.com>
2024-09-07 21:02:20 +0200

    prepare test for demo

--- src/app/pages/testing-deferrable/testing-deferrable.component.spec.ts
+++ src/app/pages/testing-deferrable/testing-deferrable.component.spec.ts
@@ -1,26 +1,37 @@
 import {
   ComponentFixture,
-  TestBed
+  DeferBlockBehavior,
+  DeferBlockFixture,
+  DeferBlockState,
+  TestBed,
 } from '@angular/core/testing';
 import { TestingDeferrableComponent } from './testing-deferrable.component';
 
 describe('PageWithDeferrableComponent', () => {
   let component: TestingDeferrableComponent;
   let fixture: ComponentFixture<TestingDeferrableComponent>;
+  // let deferredBlockFixture: DeferBlockFixture;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TestingDeferrableComponent]
+      imports: [TestingDeferrableComponent],
+      deferBlockBehavior: DeferBlockBehavior.Manual,
     }).compileComponents();
   });
 
-  beforeEach(() => {
+  beforeEach(async () => {
     fixture = TestBed.createComponent(TestingDeferrableComponent);
     component = fixture.componentInstance;
+    // deferredBlockFixture = (await fixture.getDeferBlocks())[0];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render bar chart', async () => {
+    const barChart = fixture.nativeElement.querySelector('app-bar-chart');
+    expect(barChart).toBeTruthy();
   });
 });
