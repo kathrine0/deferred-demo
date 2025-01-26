8505b2c77317d534da84a906e09ed626bcde933f
Kasia Biernat <kasia.biernat@hyland.com>
2025-01-26 13:16:24 +0100

    update spec

--- src/app/pages/testing-deferrable/testing-deferrable.component.spec.ts
+++ src/app/pages/testing-deferrable/testing-deferrable.component.spec.ts
@@ -10,7 +10,7 @@ import { TestingDeferrableComponent } from './testing-deferrable.component';
 describe('PageWithDeferrableComponent', () => {
   let component: TestingDeferrableComponent;
   let fixture: ComponentFixture<TestingDeferrableComponent>;
-  let deferredBlockFixture: DeferBlockFixture;
+  let deferredBlockFixture: DeferBlockFixture[];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -22,12 +22,12 @@ describe('PageWithDeferrableComponent', () => {
   beforeEach(async () => {
     fixture = TestBed.createComponent(TestingDeferrableComponent);
     component = fixture.componentInstance;
-    deferredBlockFixture = (await fixture.getDeferBlocks())[0];
+    deferredBlockFixture = (await fixture.getDeferBlocks());
     fixture.detectChanges();
   });
 
   it('should render deferred element', async () => {
-    await deferredBlockFixture.render(DeferBlockState.Complete);
+    await deferredBlockFixture[0].render(DeferBlockState.Complete);
     const element = fixture.nativeElement.querySelector(
       '[data-test-id="deferred-element"]',
     );
