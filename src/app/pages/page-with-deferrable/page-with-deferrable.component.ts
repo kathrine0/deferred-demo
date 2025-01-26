988ffea66431968b7f0efacabde0de2d11351494
Kasia Biernat <kasia.biernat@hyland.com>
2025-01-26 15:01:55 +0100

    add defer for testing

--- src/app/pages/page-with-deferrable/page-with-deferrable.component.ts
+++ src/app/pages/page-with-deferrable/page-with-deferrable.component.ts
@@ -1,12 +1,20 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { BarChartComponent } from '../../components/bar-chart/bar-chart.component';
+import { PlaceholderComponent } from "../../components/placeholder/placeholder.component";
+import { LoadingComponent } from "../../components/loading/loading.component";
 @Component({
   selector: 'app-page-with-deferrable',
   standalone: true,
-  imports: [BarChartComponent],
+  imports: [BarChartComponent, PlaceholderComponent, LoadingComponent],
   template: `
     <section class="elements">
-      <app-bar-chart />
+      @defer (on interaction) {
+        <app-bar-chart />
+      } @placeholder {
+        <app-placeholder />
+      } @loading {
+        <app-loading />
+      }
     </section>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
