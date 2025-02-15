6de3849b8dee81214939f690c5bca5948e7098a7
Kasia Biernat <kasia.biernat@hyland.com>
2025-02-15 18:33:49 +0100

    remove not needed components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,5 @@
 import { Routes } from '@angular/router';
 import { EagerPageComponent } from './pages/eager-page/eager-page.component';
-import { PlainDeferrableComponent } from './pages/samples/01-plain-deferrable.component';
-import { LoadingBlockComponent } from './pages/samples/02-loading-block.component';
-import { LoadingWithParamsComponent } from './pages/samples/03-loading-with-params.component';
-import { OnInteractionComponent } from './pages/samples/04-on-interaction.component';
-import { OnHoverComponent } from './pages/samples/05-on-hover.component';
-import { OnHoverOtherComponent } from './pages/samples/06-on-hover-other.component';
-import { OnViewportComponent } from './pages/samples/07-on-viewport.component';
-import { OnTimerComponent } from './pages/samples/08-on-timer.component';
-import { WhenCustomComponent } from './pages/samples/09-when-custom.component';
-import { MixupComponent } from './pages/samples/10-mixup.component';
-import { ErrorBlockComponent } from './pages/samples/11-error-block.component';
-import { MultipleBlocksComponent } from './pages/samples/12-multiple-blocks.component';
-import { PrefetchComponent } from './pages/samples/13-prefetch.component';
-import { SampleHostComponent } from './pages/samples/sample-host.component';
 import { PageWithDeferrableComponent } from './pages/page-with-deferrable/page-with-deferrable.component';
 
 export const routes: Routes = [
@@ -31,76 +17,5 @@ export const routes: Routes = [
   {
     path: 'page-with-deferrable',
     component: PageWithDeferrableComponent,
-  },
-  {
-    path: 'samples',
-    component: SampleHostComponent,
-    children: [
-      {
-        path: '1',
-        component: PlainDeferrableComponent,
-        title: '1 Plain deferrable',
-      },
-      {
-        path: '2',
-        component: LoadingBlockComponent,
-        title: '2 Loading block',
-      },
-      {
-        path: '3',
-        component: LoadingWithParamsComponent,
-        title: '3 Loading with params',
-      },
-      {
-        path: '4',
-        component: OnInteractionComponent,
-        title: '4 On interaction',
-      },
-      {
-        path: '5',
-        component: OnHoverComponent,
-        title: '5 On hover',
-      },
-      {
-        path: '6',
-        component: OnHoverOtherComponent,
-        title: '6 On hover other',
-      },
-      {
-        path: '7',
-        component: OnViewportComponent,
-        title: '7 On viewport',
-      },
-      {
-        path: '8',
-        component: OnTimerComponent,
-        title: '8 On timer',
-      },
-      {
-        path: '9',
-        component: WhenCustomComponent,
-        title: '9 When custom',
-      },
-      {
-        path: '10',
-        component: MixupComponent,
-        title: '10 Mixup',
-      },
-      {
-        path: '11',
-        component: ErrorBlockComponent,
-        title: '11 Error block',
-      },
-      {
-        path: '12',
-        component: MultipleBlocksComponent,
-        title: '12 Multiple blocks',
-      },
-      {
-        path: '13',
-        component: PrefetchComponent,
-        title: '13 Prefetch',
-      },
-    ],
   },
 ];
--- src/app/pages/page-with-deferrable/page-with-deferrable.component.ts
+++ src/app/pages/page-with-deferrable/page-with-deferrable.component.ts
@@ -1,14 +1,29 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { BarChartComponent } from '../../components/bar-chart/bar-chart.component';
+import { CountDownComponent } from '../../components/count-down/count-down.component';
+import { ErrorComponent } from "../../components/error/error.component";
+import { LoadingComponent } from '../../components/loading/loading.component';
+import { PlaceholderComponent } from '../../components/placeholder/placeholder.component';
+import { PolarChartComponent } from '../../components/polar-chart/polar-chart.component';
 @Component({
   selector: 'app-page-with-deferrable',
   standalone: true,
-  imports: [BarChartComponent],
+  imports: [
+    BarChartComponent,
+    // PlaceholderComponent,
+    // LoadingComponent,
+    // CountDownComponent,
+    // PolarChartComponent,
+    // ErrorComponent
+],
   template: `
+    <button (click)="shouldLoad = true">Load Chart</button>
     <section class="elements">
       <app-bar-chart />
     </section>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PageWithDeferrableComponent {}
+export class PageWithDeferrableComponent {
+  shouldLoad = false;
+}
